73cd4388350a73d39054fcf9400f45ed929ec686
xufumin <1925461534@qq.com>
2022-12-17 22:25:07 +0800

    hot-fix commit1

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello,Git！");
         System.out.println("Hello,Git22！");
         System.out.println("Hello,Git333！");
+        System.out.println("Hello,Git4444！");
     }
 }
